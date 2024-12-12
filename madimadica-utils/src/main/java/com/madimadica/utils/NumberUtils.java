066d10f8945e1b0f926c4579e46bb7b5da180ae1
Madi <145222821+madimadica@users.noreply.github.com>
2024-12-11 22:02:47 -0600

    NumberUtils: Add Optional<?> parsers

--- madimadica-utils/src/main/java/com/madimadica/utils/NumberUtils.java
+++ madimadica-utils/src/main/java/com/madimadica/utils/NumberUtils.java
@@ -1,6 +1,8 @@
 package com.madimadica.utils;
 
 
+import java.util.Optional;
+
 /**
  * Utilities for fast operations on primitive number types.
  */
@@ -216,6 +218,244 @@ public abstract class NumberUtils {
             a *= 10;
         }
         return a + b;
+    }
+
+    /**
+     * Attempts to parse the string argument into a byte.
+     * Any exceptional value will result in {@link Optional#empty()}.
+     *
+     * @param s the {@code String} to be parsed.
+     * @return an {@link Optional<Byte>} represented by the string argument.
+     * @see Byte#parseByte(String)
+     */
+    public static Optional<Byte> parseByte(String s) {
+        try {
+            return Optional.of(Byte.parseByte(s));
+        } catch (NumberFormatException ignored) {
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * Attempts to parse the string argument into a byte.
+     * Any exceptional value will result in {@link Optional#empty()}.
+     *
+     * @param s the {@code String} to be parsed.
+     * @param radix the radix to be used while parsing
+     * @return an {@link Optional<Byte>} represented by the string argument.
+     * @see Byte#parseByte(String, int)
+     */
+    public static Optional<Byte> parseByte(String s, int radix) {
+        try {
+            return Optional.of(Byte.parseByte(s, radix));
+        } catch (NumberFormatException ignored) {
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * Attempts to parse the string argument into a short.
+     * Any exceptional value will result in {@link Optional#empty()}.
+     *
+     * @param s the {@code String} to be parsed.
+     * @return an {@link Optional<Short>} represented by the string argument.
+     * @see Short#parseShort(String)
+     */
+    public static Optional<Short> parseShort(String s) {
+        try {
+            return Optional.of(Short.parseShort(s));
+        } catch (NumberFormatException ignored) {
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * Attempts to parse the string argument into a short.
+     * Any exceptional value will result in {@link Optional#empty()}.
+     *
+     * @param s the {@code String} to be parsed.
+     * @param radix the radix to be used while parsing
+     * @return an {@link Optional<Short>} represented by the string argument.
+     * @see Short#parseShort(String, int)
+     */
+    public static Optional<Short> parseShort(String s, int radix) {
+        try {
+            return Optional.of(Short.parseShort(s, radix));
+        } catch (NumberFormatException ignored) {
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * Attempts to parse the string argument into an integer.
+     * Any exceptional value will result in {@link Optional#empty()}.
+     *
+     * @param s the {@code String} to be parsed.
+     * @return an {@link Optional<Integer>} represented by the string argument.
+     * @see Integer#parseInt(String)
+     */
+    public static Optional<Integer> parseInt(String s) {
+        try {
+            return Optional.of(Integer.parseInt(s));
+        } catch (NumberFormatException ignored) {
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * Attempts to parse the string argument into an integer.
+     * Any exceptional value will result in {@link Optional#empty()}.
+     *
+     * @param s the {@code String} to be parsed.
+     * @param radix the radix to be used while parsing
+     * @return an {@link Optional<Integer>} represented by the string argument.
+     * @see Integer#parseInt(String, int)
+     */
+    public static Optional<Integer> parseInt(String s, int radix) {
+        try {
+            return Optional.of(Integer.parseInt(s, radix));
+        } catch (NumberFormatException ignored) {
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * Attempts to parse the string argument into a long.
+     * Any exceptional value will result in {@link Optional#empty()}.
+     *
+     * @param s the {@code String} to be parsed.
+     * @return an {@link Optional<Long>} represented by the string argument.
+     * @see Long#parseLong(String)
+     */
+    public static Optional<Long> parseLong(String s) {
+        try {
+            return Optional.of(Long.parseLong(s));
+        } catch (NumberFormatException ignored) {
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * Attempts to parse the string argument into a long.
+     * Any exceptional value will result in {@link Optional#empty()}.
+     *
+     * @param s the {@code String} to be parsed.
+     * @param radix the radix to be used while parsing
+     * @return an {@link Optional<Long>} represented by the string argument.
+     * @see Long#parseLong(String, int)
+     */
+    public static Optional<Long> parseLong(String s, int radix) {
+        try {
+            return Optional.of(Long.parseLong(s, radix));
+        } catch (NumberFormatException ignored) {
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * Attempts to parse the string argument into a boolean.
+     * Only the values {@code "true"} and {@code "false"} (case-insensitive) are mapped
+     * to a {@link Boolean} value, otherwise the result is {@link Optional#empty()} (including for {@code null}).
+     * <br>
+     * This differs from {@link Boolean#parseBoolean(String)}
+     * by separating a {@code false} return from an error ({@code Optional.empty()}).
+     * @param s the {@code String} to be parsed.
+     * @return an {@link Optional} {@code boolean} represented by the string argument.
+     */
+    public static Optional<Boolean> parseBoolean(String s) {
+        if (s == null) {
+            return Optional.empty();
+        }
+        switch (s.toLowerCase()) {
+            case "true":
+                return Optional.of(Boolean.TRUE);
+            case "false":
+                return Optional.of(Boolean.FALSE);
+            default:
+                return Optional.empty();
+        }
+    }
+
+    /**
+     * Attempts to parse the string argument into a boolean.
+     * Only the values {@code "1"} and {@code "0"} are mapped
+     * to {@code true} and {@code false} respectively.
+     * Otherwise, the result is {@link Optional#empty()}.
+     *
+     * @param s the {@code String} to be parsed.
+     * @return an {@link Optional} {@code boolean} represented by the string argument.
+     */
+    public static Optional<Boolean> parseBit(String s) {
+        if (s == null) {
+            return Optional.empty();
+        }
+        switch (s) {
+            case "1":
+                return Optional.of(Boolean.TRUE);
+            case "0":
+                return Optional.of(Boolean.FALSE);
+            default:
+                return Optional.empty();
+        }
+    }
+
+    /**
+     * Attempts to parse the string argument into a boolean.
+     * Only the values {@code "false"} (case-insensitive) and {@code "0"} are mapped to {@code false},
+     * and the values {@code "true"} (case-insensitive) and {@code "1"} are mapped to {@code true},
+     * everything else is {@link Optional#empty()}.
+     *
+     * @param s the {@code String} to be parsed.
+     * @return an {@link Optional} {@code boolean} represented by the string argument.
+     * @see NumberUtils#parseBoolean(String)
+     * @see NumberUtils#parseBit(String)
+     */
+    public static Optional<Boolean> parseBooleanOrBit(String s) {
+        if (s == null) {
+            return Optional.empty();
+        }
+        switch (s.toLowerCase()) {
+            case "1":
+            case "true":
+                return Optional.of(Boolean.TRUE);
+            case "0":
+            case "false":
+                return Optional.of(Boolean.FALSE);
+            default:
+                return Optional.empty();
+        }
+    }
+
+    /**
+     * Attempts to parse the string argument into a float.
+     * Any exceptional value will result in {@link Optional#empty()}.
+     *
+     * @param s the {@code String} to be parsed.
+     * @return an {@link Optional<Short>} represented by the string argument.
+     * @see Float#parseFloat(String)
+     */
+    public static Optional<Float> parseFloat(String s) {
+        try {
+            return Optional.of(Float.parseFloat(s));
+        } catch (Exception ignored) {
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * Attempts to parse the string argument into a double.
+     * Any exceptional value will result in {@link Optional#empty()}.
+     *
+     * @param s the {@code String} to be parsed.
+     * @return an {@link Optional<Double>} represented by the string argument.
+     * @see Double#parseDouble(String)
+     */
+    public static Optional<Double> parseDouble(String s) {
+        try {
+            return Optional.of(Double.parseDouble(s));
+        } catch (Exception ignored) {
+            return Optional.empty();
+        }
     }
 
 }
--- madimadica-utils/src/test/java/com/madimadica/utils/NumberUtilsTest.java
+++ madimadica-utils/src/test/java/com/madimadica/utils/NumberUtilsTest.java
@@ -2,6 +2,8 @@ package com.madimadica.utils;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumberUtilsTest {
@@ -81,6 +83,171 @@ class NumberUtilsTest {
         assertEquals(1234, NumberUtils.concat(123L, 4L));
         assertEquals(123456, NumberUtils.concat(123L, 456L));
         assertEquals(123_456_789_987_654_321L, NumberUtils.concat(123_456_789L, 987_654_321L));
+    }
+
+    @Test
+    void parseByte() {
+        assertTrue(NumberUtils.parseByte(null).isEmpty());
+        assertTrue(NumberUtils.parseByte("foo").isEmpty());
+        assertTrue(NumberUtils.parseByte("1234567").isEmpty());
+
+        Optional<Byte> testVal = NumberUtils.parseByte("123");
+        assertTrue(testVal.isPresent());
+        assertEquals((byte) 123, testVal.get());
+    }
+
+    @Test
+    void parseByteRadix() {
+        assertTrue(NumberUtils.parseByte(null, 16).isEmpty());
+        assertTrue(NumberUtils.parseByte("foo", 16).isEmpty());
+        assertTrue(NumberUtils.parseByte("FFF", 16).isEmpty());
+
+        Optional<Byte> testVal = NumberUtils.parseByte("F", 16);
+        assertTrue(testVal.isPresent());
+        assertEquals((byte) 0xF, testVal.get());
+    }
+
+    @Test
+    void parseShort() {
+        assertTrue(NumberUtils.parseShort(null).isEmpty());
+        assertTrue(NumberUtils.parseShort("foo").isEmpty());
+        assertTrue(NumberUtils.parseShort("1234567").isEmpty());
+
+        Optional<Short> testVal = NumberUtils.parseShort("123");
+        assertTrue(testVal.isPresent());
+        assertEquals((short) 123, testVal.get());
+    }
+
+    @Test
+    void parseShortRadix() {
+        assertTrue(NumberUtils.parseShort(null, 16).isEmpty());
+        assertTrue(NumberUtils.parseShort("foo", 16).isEmpty());
+        assertTrue(NumberUtils.parseShort("FFFFF", 16).isEmpty());
+
+        Optional<Short> testVal = NumberUtils.parseShort("FF", 16);
+        assertTrue(testVal.isPresent());
+        assertEquals((short) 0xFF, testVal.get());
+    }
+
+    @Test
+    void parseInt() {
+        assertTrue(NumberUtils.parseInt(null).isEmpty());
+        assertTrue(NumberUtils.parseInt("foo").isEmpty());
+        assertTrue(NumberUtils.parseInt("123456789123456789").isEmpty());
+
+        Optional<Integer> testVal = NumberUtils.parseInt("123456789");
+        assertTrue(testVal.isPresent());
+        assertEquals(123456789, testVal.get());
+    }
+
+    @Test
+    void parseIntRadix() {
+        assertTrue(NumberUtils.parseInt(null, 16).isEmpty());
+        assertTrue(NumberUtils.parseInt("foo", 16).isEmpty());
+        assertTrue(NumberUtils.parseInt("FFFFFFFFF", 16).isEmpty());
+
+        Optional<Integer> testVal = NumberUtils.parseInt("FFFF", 16);
+        assertTrue(testVal.isPresent());
+        assertEquals(0xFFFF, testVal.get());
+    }
+
+    @Test
+    void parseLong() {
+        assertTrue(NumberUtils.parseLong(null).isEmpty());
+        assertTrue(NumberUtils.parseLong("foo").isEmpty());
+        assertTrue(NumberUtils.parseLong("123456789123456789123456789").isEmpty());
+
+        Optional<Long> testVal = NumberUtils.parseLong("123456789123456789");
+        assertTrue(testVal.isPresent());
+        assertEquals(123456789123456789L, testVal.get());
+    }
+
+    @Test
+    void parseLongRadix() {
+        assertTrue(NumberUtils.parseLong(null, 16).isEmpty());
+        assertTrue(NumberUtils.parseLong("foo", 16).isEmpty());
+        assertTrue(NumberUtils.parseLong("FFFFFFFFFFFFFFFFF", 16).isEmpty());
+
+        Optional<Long> testVal = NumberUtils.parseLong("FFFF0000FFFF", 16);
+        assertTrue(testVal.isPresent());
+        assertEquals(0xFFFF0000FFFFL, testVal.get());
+    }
+
+    @Test
+    void parseBoolean() {
+        assertTrue(NumberUtils.parseBoolean("TRUE").get());
+        assertTrue(NumberUtils.parseBoolean("true").get());
+        assertTrue(NumberUtils.parseBoolean("TrUE").get());
+        assertTrue(NumberUtils.parseBoolean("TruE").get());
+
+        assertFalse(NumberUtils.parseBoolean("false").get());
+        assertFalse(NumberUtils.parseBoolean("falSE").get());
+        assertFalse(NumberUtils.parseBoolean("FALSE").get());
+        assertFalse(NumberUtils.parseBoolean("FaLsE").get());
+
+        assertTrue(NumberUtils.parseBoolean(null).isEmpty());
+        assertTrue(NumberUtils.parseBoolean("").isEmpty());
+        assertTrue(NumberUtils.parseBoolean("foo").isEmpty());
+        assertTrue(NumberUtils.parseBoolean("0").isEmpty());
+        assertTrue(NumberUtils.parseBoolean("1").isEmpty());
+    }
+
+    @Test
+    void parseBit() {
+        assertFalse(NumberUtils.parseBit("0").get());
+        assertTrue(NumberUtils.parseBit("1").get());
+
+        assertTrue(NumberUtils.parseBit(null).isEmpty());
+        assertTrue(NumberUtils.parseBit("false").isEmpty());
+        assertTrue(NumberUtils.parseBit("-0").isEmpty());
+        assertTrue(NumberUtils.parseBit("true").isEmpty());
+        assertTrue(NumberUtils.parseBit("-1").isEmpty());
+        assertTrue(NumberUtils.parseBit("").isEmpty());
+        assertTrue(NumberUtils.parseBit("foo").isEmpty());
+    }
+
+    @Test
+    void parseBooleanOrBit() {
+        assertTrue(NumberUtils.parseBooleanOrBit("TRUE").get());
+        assertTrue(NumberUtils.parseBooleanOrBit("true").get());
+        assertTrue(NumberUtils.parseBooleanOrBit("TrUE").get());
+        assertTrue(NumberUtils.parseBooleanOrBit("TruE").get());
+
+        assertFalse(NumberUtils.parseBooleanOrBit("false").get());
+        assertFalse(NumberUtils.parseBooleanOrBit("falSE").get());
+        assertFalse(NumberUtils.parseBooleanOrBit("FALSE").get());
+        assertFalse(NumberUtils.parseBooleanOrBit("FaLsE").get());
+
+        assertFalse(NumberUtils.parseBooleanOrBit("0").get());
+        assertTrue(NumberUtils.parseBooleanOrBit("1").get());
+
+        assertTrue(NumberUtils.parseBooleanOrBit(null).isEmpty());
+        assertTrue(NumberUtils.parseBooleanOrBit("-0").isEmpty());
+        assertTrue(NumberUtils.parseBooleanOrBit("-1").isEmpty());
+        assertTrue(NumberUtils.parseBooleanOrBit("").isEmpty());
+        assertTrue(NumberUtils.parseBooleanOrBit("foo").isEmpty());
+    }
+
+    @Test
+    void parseFloat() {
+        assertTrue(NumberUtils.parseFloat(null).isEmpty());
+        assertTrue(NumberUtils.parseFloat("foo").isEmpty());
+        assertTrue(NumberUtils.parseFloat("").isEmpty());
+
+        Optional<Float> testVal = NumberUtils.parseFloat("123.45");
+        assertTrue(testVal.isPresent());
+        assertEquals(123.45f, testVal.get());
+    }
+
+    @Test
+    void parseDouble() {
+        assertTrue(NumberUtils.parseDouble(null).isEmpty());
+        assertTrue(NumberUtils.parseDouble("foo").isEmpty());
+        assertTrue(NumberUtils.parseDouble("").isEmpty());
+
+        Optional<Double> testVal = NumberUtils.parseDouble("123.45");
+        assertTrue(testVal.isPresent());
+        assertEquals(123.45, testVal.get());
     }
 
 }
